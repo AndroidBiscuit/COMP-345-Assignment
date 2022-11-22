cef2bd7d27682ef1bd65be0b366adb6f6a14544d
Faizan Ahmad <51469062+AndroidBiscuit@users.noreply.github.com>
2022-11-22 05:57:06 -0500

    Added code structure and core files

---
+++ RiskProject/RiskProject/PlayerStrategies.cpp
@@ -0,0 +1,82 @@
+#include "PlayerStrategies.h"
+
+//_________________________________________________________________________________________________
+// Human player strategy
+//-------------------------------------------------------------------------------------------------
+
+// Issue order
+void HumanPlayerStrategy::issueOrder(Player* player, string order) {
+	//
+}
+
+// Return the attack list of selected player
+vector<Territory*> HumanPlayerStrategy::toAttack(Player* player) {
+	//
+}
+
+// Return the defend list of selected player
+vector<Territory*> HumanPlayerStrategy::toDefend(Player* player) {
+	//
+}
+
+
+//_________________________________________________________________________________________________
+// Aggressive player strategy
+//-------------------------------------------------------------------------------------------------
+
+// Issue order
+void HumanPlayerStrategy::issueOrder(Player* player, string order) {
+	//
+}
+
+// Return the attack list of selected player
+vector<Territory*> HumanPlayerStrategy::toAttack(Player* player) {
+	//
+}
+
+// Return the defend list of selected player
+vector<Territory*> HumanPlayerStrategy::toDefend(Player* player) {
+	//
+}
+
+
+//_________________________________________________________________________________________________
+// Benevolent player strategy
+//-------------------------------------------------------------------------------------------------
+
+// Issue order
+void BenevolentPlayerStrategy::issueOrder(Player* player, string order) {
+	//
+}
+
+// Return the attack list of selected player
+vector<Territory*> BenevolentPlayerStrategy::toAttack(Player* player) {
+	//
+}
+
+// Return the defend list of selected player
+vector<Territory*> BenevolentPlayerStrategy::toDefend(Player* player) {
+	//
+}
+
+
+//_________________________________________________________________________________________________
+// Neutral player strategy
+//-------------------------------------------------------------------------------------------------
+
+// Issue order
+void HumanPlayerStrategy::issueOrder(Player* player, string order) {
+	//
+}
+
+// Return the attack list of selected player
+vector<Territory*> NeutralPlayerStrategy::toAttack(Player* player) {
+	//
+}
+
+// Return the defend list of selected player
+vector<Territory*> NeutralPlayerStrategy::toDefend(Player* player) {
+	//
+}
+
+//TODO cheater player?
